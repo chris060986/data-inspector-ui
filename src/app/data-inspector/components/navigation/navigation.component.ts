86fd8c5928074ce014505accc4e3a1450c97e19e
Andi Di <andreasdirr@gmx.net>
2018-06-28 17:42:00 +0200

    fullscreen toggle

--- src/app/data-inspector/components/navigation/navigation.component.ts
+++ src/app/data-inspector/components/navigation/navigation.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'diu-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  selector: "diu-navigation",
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.css"]
 })
 export class NavigationComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
+  fullscreen: boolean = false;
 
   ngOnInit() {
+    document.addEventListener(
+      "fullscreenchange",
+      () => {
+        this.fullscreen = !this.fullscreen;
+      },
+      false
+    );
+    document.addEventListener(
+      "mozfullscreenchange",
+      () => {
+        this.fullscreen = !this.fullscreen;
+      },
+      false
+    );
+    document.addEventListener(
+      "webkitfullscreenchange",
+      () => {
+        this.fullscreen = !this.fullscreen;
+      },
+      false
+    );
   }
 
+  enterFullscreen() {
+    let element = document.documentElement;
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    }
+  }
+
+  exitFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
 }
